b4f9506b6ed12f9749c88595499bfe48feca126e
Walter Perry <walterjamesperry@gmail.com>
2016-10-23 11:51:12 -0500

    Website template and what I have so far

--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -1,23 +1,23 @@
 angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
 
-	$routeProvider
+    $routeProvider
 
-		// home page
-		.when('/', {
-			templateUrl: 'views/home.html',
-			controller: 'MainController'
-		})
+    // home page
+        .when('/', {
+            templateUrl: 'views/home.html',
+            controller: 'MainController'
+        })
+        // nerds page that will use the NerdController
+        .when('/nerds', {
+            templateUrl: 'views/nerd.html',
+            controller: 'NerdController'
+        })
+        // geekss page that will use the GeekController
+        .when('/geeks', {
+            templateUrl: 'views/geek.html',
+            controller: 'GeekController'
+        });
 
-		.when('/nerds', {
-			templateUrl: 'views/nerd.html',
-			controller: 'NerdController'
-		})
-
-		.when('/geeks', {
-			templateUrl: 'views/geek.html',
-			controller: 'GeekController'	
-		});
-
-	$locationProvider.html5Mode(true);
+    $locationProvider.html5Mode(true);
 
 }]);
